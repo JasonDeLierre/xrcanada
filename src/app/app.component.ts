84d2144022cce9a016aae40e67614fcd56cc98e0
Rom Grk <romgrk.cc@gmail.com>
2019-04-09 14:02:31 -0400

    refactor structure & import footer from blog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,43 +6,30 @@ import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
+  private activeRoute: string;
 
   constructor(
     private router: Router,
     private trans: TranslateService
-  ){
-
+  ) {
     trans.setDefaultLang('en')
 
+    this.activeRoute = location.pathname
   }
 
-  translate(x){
+  translate(x) {
     console.log('x', x)
     if(x=='en') this.trans.setDefaultLang('en')
     else        this.trans.setDefaultLang('fr')
   }
 
-  route(x:string){
-
+  route(x:string) {
     console.log('x', x)
-
-    if(x=='m')
-      this.router.navigate(['/'])
-    if(x=='w')
-      this.router.navigate(['/who'])
-    if(x=='d')
-      this.router.navigate(['/demands'])
-    if(x=='p')
-      this.router.navigate(['/principles'])
-    if(x=='j')
-      this.router.navigate(['/join'])
-    if(x=='a')
-      this.router.navigate(['/media'])
-    if(x=='v')
-      this.router.navigate(['/provinces'])
-
+    this.router.navigate([x])
+    this.activeRoute = x
   }
 }
