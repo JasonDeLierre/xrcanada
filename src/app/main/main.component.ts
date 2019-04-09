4d55c93f4ea369f103adf24fbc9537841b342a9b
Rom Grk <romgrk.cc@gmail.com>
2019-04-09 15:47:16 -0400

    app & main: improve styling

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -5,7 +5,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.css']
+  styleUrls: ['./main.component.scss']
 })
 export class MainComponent implements OnInit {
 
