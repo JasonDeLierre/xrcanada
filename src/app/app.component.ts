6149f3a9fd920bff5325dabd0d0b1c37f1aa2338
Rom Grk <romgrk.cc@gmail.com>
2019-04-10 18:54:17 -0400

    quebec: add content (untranslated)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class AppComponent {
 
-  private activeRoute: string;
+  public activeRoute: string;
 
   constructor(
     private router: Router,
