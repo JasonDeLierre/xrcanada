2ac7437e57d50088bfff1cdbfdb6f978dc29acc5
Rom Grk <romgrk.cc@gmail.com>
2019-04-14 22:13:25 -0400

    quebec: insert actionnetwork widget

--- src/app/provinces/quebec/quebec.component.ts
+++ src/app/provinces/quebec/quebec.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef } from '@angular/core';
+
+let nextId = 1;
 
 @Component({
   selector: 'app-quebec',
   templateUrl: './quebec.component.html',
   styleUrls: ['./quebec.component.css']
 })
-export class QuebecComponent implements OnInit {
+export class QuebecComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  id: string;
+
+  constructor(private elementRef: ElementRef) {
+    this.id = `actionnetwork_widget_${nextId++}`;
+  }
 
   ngOnInit() {
   }
 
+  ngAfterViewInit() {
+    const url = `https://actionnetwork.org/widgets/v3/fundraising/soutenez-financierement-extinction-rebellion-quebec?format=js&source=widget&can_widget_id=${this.id}`;
+    const script = document.createElement('script');
+    script.src = url;
+    this.elementRef.nativeElement.appendChild(script);
+  }
 }
